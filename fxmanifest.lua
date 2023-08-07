d143ad390a70b2430f65b6add660815f58e6f7b9
Techy-Dev <129864676+Techy-Dev@users.noreply.github.com>
2023-08-07 10:24:11 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+game { 'gta5' }
+
+author 'Goonie Customs'
+description 'Custom Made Nissan 370Z Hand Picked By techy.rp'
+
+files {
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+    'data/carcols.meta',
+    'data/handling.meta',
+    'data/vehiclelayouts.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+
+client_script 'vehicle_names.lua'
